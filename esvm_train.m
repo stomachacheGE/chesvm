170efcf3cc7ad6e83a15c32fb4627429e2d9f22b
stomachacheGE <liangchengfu001@gmail.com>
2016-06-16 13:43:54 +0200

    stable version running maritime

--- esvm_train.m
+++ esvm_train.m
@@ -17,7 +17,6 @@ use_algorithm = algorithm;
 calibration = calibration;
 hard_negative = hard_negative;
 
-
 [train_datas, test_datas] = esvm_initialize_features(datasets_info, ...
                                                      use_feature,use_algorithm,params);
 
@@ -26,22 +25,23 @@ if strcmp(use_algorithm,'svm')
     prediction = esvm_predict_svm(linearSVMmodel, test_datas);
 else
     [models, neg_set] = esvm_train_initialization(train_datas, use_feature);
+    
     if hard_negative
-        new_models = esvm_train_exemplars_hn(models, neg_set, use_feature,params);
+        new_models = esvm_train_exemplars_hn(models, neg_set, use_feature, params);
     else
-        new_models = esvm_train_exemplars(models, neg_set, use_algorithm, use_feature,params);
+        new_models = esvm_train_exemplars(models, neg_set, use_feature, params);
     end
     
-    prediction = esvm_predict(new_models,test_datas, use_feature, params);
+    prediction = esvm_predict(new_models,test_datas, use_feature, hard_negative, params);
     
     if calibration 
-        cal_matrix = esvm_perform_calibration(new_models, train_datas, use_feature, params);
-        prediction = esvm_apply_sigmoid(cal_matrix, test_datas, use_feature, params);
+        cal_matrix = esvm_perform_calibration(new_models, train_datas, use_feature, hard_negative, params);
+        prediction = esvm_apply_sigmoid(cal_matrix, test_datas, use_feature, hard_negative, params);
     end
 end
 
 ap_res = esvm_evaluate_AP(prediction, test_datas, use_algorithm, ...
-                          use_feature, params, calibration);
+                          use_feature, calibration, hard_negative, params);
 
 for i = 1:length(ap_res)
     fprintf(1, 'Class %s has an average precision of %f \n', ...
